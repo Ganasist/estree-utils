9ab9eb34b8e5667bfbffe02855dd7d4936ea3d5a
Jonathan Huang <grokut@gmail.com>
2015-11-25 20:55:12 -0500

    test/ update to test multiple name args

--- tests/filterTreeForMethodsAndFunctionsNamed.spec.js
+++ tests/filterTreeForMethodsAndFunctionsNamed.spec.js
@@ -2,14 +2,15 @@ import {expect} from 'chai';
 import {all, filter} from 'ramda';
 import filterTreeForMethodsAndFunctionsNamed from 'src/filterTreeForMethodsAndFunctionsNamed';
 import isMethodOrFunctionNamed from 'src/isMethodOrFunctionNamed';
+import mixAST from 'assets/ast-mix-example';
 import ASTJX from 'assets/ast-example';
 import ASTJS from 'assets/ast-small-example';
 
 describe('filterTreeForMethodsAndFunctionsNamed', () => {
-  it('returns a collection of all methods and functions in a tree named the same as the argument', () => {
+  it('returns a collection of all named methods and functions in a tree', () => {
 
-    const getAllGettext = filterTreeForMethodsAndFunctionsNamed('__');
-    const allGettext = getAllGettext(ASTJX.body);
+    let getAllGettext = filterTreeForMethodsAndFunctionsNamed('__');
+    let allGettext = getAllGettext(ASTJX.body);
 
     const getAllHello = filterTreeForMethodsAndFunctionsNamed('hello');
     const allHello = getAllHello(ASTJS.body);
@@ -18,5 +19,17 @@ describe('filterTreeForMethodsAndFunctionsNamed', () => {
     expect(allGettext.length).to.be.equal(2);
     expect(all(isMethodOrFunctionNamed('hello'))(allHello)).to.be.true;
     expect(allHello.length).to.be.equal(1);
+  });
+
+  it('can take multiple name arguments', () => {
+    const names = ['__', 'translate', 'translateMethod'];
+    let getAllGettext = filterTreeForMethodsAndFunctionsNamed(...names);
+    let allGettext = getAllGettext(mixAST.body);
+    const isi18n = isMethodOrFunctionNamed('__')
+    const alli18n = filter(isi18n)(allGettext);
+
+    expect(alli18n.length).to.be.equal(3)
+    expect(all(isMethodOrFunctionNamed(...names))(allGettext)).to.be.true;
+    expect(allGettext.length).to.be.equal(6);
   });
 });
